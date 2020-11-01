38a00179e76081e18d707d9015664e28ea2fc0db
WGX <657826620@qq.com>
2020-11-01 11:23:16 +0800

    Initial commit

---
+++ src/com/atguigu/Idea.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class Idea {
+    public static void main(String[] args) {
+        System.out.println("你好啊！");
+        System.out.println("你好啊！");
+        System.out.println("wgxdev2！");
+        System.out.println("wgxdev2！");
+    }
+}
