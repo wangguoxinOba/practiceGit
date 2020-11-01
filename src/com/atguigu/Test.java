73756fc9baaf47a8617c44e863ae5d6add255c8f
WGX <657826620@qq.com>
2020-11-01 11:27:37 +0800

    Test分支修改Test.java文件并提交。

--- src/com/atguigu/Test.java
+++ src/com/atguigu/Test.java
@@ -2,14 +2,6 @@ package com.atguigu;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("这是Egit!");
-        System.out.println("这是02t!");
-        System.out.println("我把远程仓库从https修改成ssh的!");
-        System.out.println("这是使用git bash克隆下来之后添加的代码！");
-        System.out.println("这是我要测试一下 git push origin master这命令！");
-        System.out.println("这是在Eclipse中将https修改成ssh之后的修改！");
-        System.out.println("这是看看Team synchronized视图中的变化！");
-        System.out.println("kepler版本使用ssh失败，mars可以，，，应该是版本的问题，具体的就不深究了！");
-.0.0.0.0.0.0
+		System.out.println("Test分支修改!");
 	}
 }
